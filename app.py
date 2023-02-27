bfd27084a58b9bc989e2c2443b18a7302c27bfc7
Brigham Andersen <brighamband@gmail.com>
2023-02-27 14:42:29 -0700

    First push since switching to legit no end insight ui

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import os
-from flask import Flask, render_template, request, abort, url_for, redirect
+from flask import Flask, session, flash, render_template, request, abort, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from dotenv import load_dotenv
 
+load_dotenv()
 
 app = Flask(__name__)
 
+app.secret_key = os.environ.get('SECRET_KEY')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app.db')
 
@@ -30,11 +33,30 @@ class Insight(db.Model):
 
 HARD_CODED_CURR_USER = User.query.get(2)  # guest
 
-
+# Login Page
 @app.route("/")
 def index():
-    all_insights = Insight.query.all()
-    return render_template("index.html", insights=all_insights)
+    return render_template("index.html")
+
+
+# Login Function
+@app.route("/login", methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    user = User.query.filter_by(username=username, password=password).first()
+    print(user.username)
+    if user:
+        session['user_id'] = user.id  # store user's ID in the session
+        return redirect(url_for('feed'))
+    else:
+        flash('Invalid username or password', 'error')
+
+
+@app.route("/feed")
+def feed():
+    all_insights = Insight.query.order_by(Insight.date.desc()).all()
+    return render_template("feed.html", insights=all_insights)
 
 
 @app.route("/author")
@@ -50,21 +72,24 @@ def profile(username=None):
         return render_template("profile.html", user=user)
 
 
+# Create Page
 @app.route("/create")
 def create():
     return render_template("create.html")
 
+
+# Share Function
 @app.route("/share", methods=['POST'])
 def share():
-    title = request.form["title"]
-    body = request.form["body"]
+    title = request.form["insight-title"]
+    body = request.form["insight-body"]
     author = HARD_CODED_CURR_USER
     if title and body and author:
         insight = Insight(title=title, body=body, author_id=author.id)
         db.session.add(insight)
         db.session.commit()
     # Go back to home screen
-    return redirect(url_for("index"))
+    return redirect(url_for("feed"))
 
 
 if __name__ == "__main__":
